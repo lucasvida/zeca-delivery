218884f622687e8e6b8fdf8e530ffdf2a1b26073
Lucas Vida <lucasvida@gmail.com>
2024-01-26 00:11:03 -0300

    Ajustes no Carrinho | Inclusão do SweetAlert JS

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -1,24 +1,54 @@
-let carrinhoString = sessionStorage.getItem('carrinho');
+let carrinhoString = sessionStorage.getItem("carrinho");
 let carrinhoGerado = JSON.parse(carrinhoString);
-let carrinhoCompras = document.querySelector('.itens-carrinho')
+let carrinhoCompras = document.querySelector(".itens-carrinho");
+const subtotal = document.querySelector(".subtotal");
+const subtotalItens = document.querySelector(".subtotal-itens");
+const itensCarrinhoQuantidade = document.querySelector(".icone-carrinho");
+let somaPrecos = 0;
 
-window.addEventListener("load", ()=> {
-    carrinhoGerado != null ? renderizaCarrinho() : alert('vazio');
-  });
+window.addEventListener("load", () => {
+  carrinhoGerado != null ? renderizaCarrinho() : alert("vazio");
+});
 
-  function renderizaCarrinho (){
-    for (let produto of carrinhoGerado){
-        carrinhoCompras.innerHTML += `
+function renderizaCarrinho() {
+  for (let produto of carrinhoGerado) {
+    carrinhoCompras.innerHTML += `
         <td>
             <div class="produto-listado">
-            <img src="${produto.imagem}" alt="${produto.nome}" class="img-tumb-carrinho">
+            <img src="${produto.imagem}" alt="${
+      produto.nome
+    }" class="img-tumb-carrinho">
                 <div class="info-produto">
                 <b>${produto.nome}</b><br>
-                <span class="categoria">${produto.categoria} | ${produto.sku}</span><br><br>
-                <span class="preco">${produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span><br>
+                <span class="categoria">${produto.categoria} | ${
+      produto.sku
+    }</span><br><br>
+                <span class="preco">${produto.preco.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}<br>
+                
                 </div>
             </div>
+            <hr>
         </td>
     `;
-    }
   }
+}
+
+for (let i = 0; i < carrinhoGerado.length; i++) {
+  somaPrecos += carrinhoGerado[i].preco;
+}
+subtotalItens.innerText = carrinhoGerado.length;
+subtotalItens.classList.add("valor-da-compra");
+
+//Fazer um if
+itensCarrinhoQuantidade.innerText = carrinhoGerado.length;
+
+subtotal.innerHTML = `<div class="valor-da-compra">${somaPrecos.toLocaleString(
+  "pt-BR",
+  {
+    style: "currency",
+    currency: "BRL",
+  }
+)}</div>`;
--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,148 +1,159 @@
 let produtos = [
-    {
-      id: '01',
-      nome: 'Cerveja Amstel - 600ml',
-      sku: 'AMS600ML',
-      preco: 6.87,
-      categoria: 'Cerveja',
-      imagem: '../assets/img/produtos/cerveja-amstel-600-ml.png',
-      quantidade: 1,
-    },
-     {
-      id: '02',
-      nome: 'Cerveja Heineken - Long Neck - 350ml',
-      sku: 'HEI350ML',
-      preco: 5.55,
-      categoria: 'Cerveja',
-      imagem: '../assets/img/produtos/cerveja-heineken-350-ml.png',
-      quantidade: 1,
-    },
-    {
-      id: '03',
-      nome: 'Refrigerante Lata Pepsi - 350ml',
-      sku: 'PEPSI350ML',
-      preco: 3.99,
-      categoria: 'Refrigerante',
-      imagem: '../assets/img/produtos/refri-pepsi-350-ml.png',
-      quantidade: 1,
-    },
-     {
-      id: '04',
-      nome: 'Água Mineral - Sem Gás - 500ml',
-      sku: 'AGUSG500ML',
-      preco: 3.50,
-      categoria: 'Mercearia',
-      imagem: '../assets/img/produtos/agua-mineral-sem-gas-500ml.png',
-      quantidade: 1,
-    },
-    {
-      id: '05',
-      nome: 'Água Mineral - Com Gás - 500ml',
-      sku: 'AGUCG500ML',
-      preco: 4.00,
-      categoria: 'Mercearia',
-      imagem: '../assets/img/produtos/agua-mineral-com-gas-500ml.png',
-      quantidade: 1,
-    },
-    {
-      id: '06',
-      nome: 'Pacote de Gelo - Escama - 4kg',
-      sku: 'GELO4KG',
-      preco: 10.00,
-      categoria: 'Mercearia',
-      imagem: '../assets/img/produtos/pacote-gelo-4-kg.png',
-      quantidade: 1,
-    },
-    {
-      id: '07',
-      nome: 'Pacote de Gelo - Escama - 10kg',
-      sku: 'GELO10KG',
-      preco: 14.99,
-      categoria: 'Mercearia',
-      imagem: '../assets/img/produtos/pacote-gelo-10-kg.png',
-      quantidade: 1,
-    },
-    {
-      id: '08',
-      nome: 'Pacote de Carvão - 3kg',
-      sku: 'CAR3KG',
-      preco: 13.99,
-      categoria: 'Mercearia',
-      imagem: '../assets/img/produtos/pacote-carvao-3-kg.png',
-      quantidade: 1,
-    }
-  ]
-  
+  {
+    id: "01",
+    nome: "Cerveja Amstel - 600ml",
+    sku: "AMS600ML",
+    preco: 6.87,
+    categoria: "Cerveja",
+    imagem: "../assets/img/produtos/cerveja-amstel-600-ml.png",
+    quantidade: 1,
+  },
+  {
+    id: "02",
+    nome: "Cerveja Heineken - Long Neck - 350ml",
+    sku: "HEI350ML",
+    preco: 5.55,
+    categoria: "Cerveja",
+    imagem: "../assets/img/produtos/cerveja-heineken-350-ml.png",
+    quantidade: 1,
+  },
+  {
+    id: "03",
+    nome: "Refrigerante Lata Pepsi - 350ml",
+    sku: "PEPSI350ML",
+    preco: 3.99,
+    categoria: "Refrigerante",
+    imagem: "../assets/img/produtos/refri-pepsi-350-ml.png",
+    quantidade: 1,
+  },
+  {
+    id: "04",
+    nome: "Água Mineral - Sem Gás - 500ml",
+    sku: "AGUSG500ML",
+    preco: 3.5,
+    categoria: "Mercearia",
+    imagem: "../assets/img/produtos/agua-mineral-sem-gas-500ml.png",
+    quantidade: 1,
+  },
+  {
+    id: "05",
+    nome: "Água Mineral - Com Gás - 500ml",
+    sku: "AGUCG500ML",
+    preco: 4.0,
+    categoria: "Mercearia",
+    imagem: "../assets/img/produtos/agua-mineral-com-gas-500ml.png",
+    quantidade: 1,
+  },
+  {
+    id: "06",
+    nome: "Pacote de Gelo - Escama - 4kg",
+    sku: "GELO4KG",
+    preco: 10.0,
+    categoria: "Mercearia",
+    imagem: "../assets/img/produtos/pacote-gelo-4-kg.png",
+    quantidade: 1,
+  },
+  {
+    id: "07",
+    nome: "Pacote de Gelo - Escama - 10kg",
+    sku: "GELO10KG",
+    preco: 14.99,
+    categoria: "Mercearia",
+    imagem: "../assets/img/produtos/pacote-gelo-10-kg.png",
+    quantidade: 1,
+  },
+  {
+    id: "08",
+    nome: "Pacote de Carvão - 3kg",
+    sku: "CAR3KG",
+    preco: 13.99,
+    categoria: "Mercearia",
+    imagem: "../assets/img/produtos/pacote-carvao-3-kg.png",
+    quantidade: 1,
+  },
+];
+
 let carrinho = [];
-const grid = document.querySelector('.produtos');
-  
-  
+const grid = document.querySelector(".produtos");
+
 function renderizaProduto(produto) {
-  let criaEl = document.createElement('div');
-    criaEl.innerHTML = `
+  let criaEl = document.createElement("div");
+  criaEl.innerHTML = `
       <img src="${produto.imagem}" alt="${produto.nome}">
       <b>${produto.nome}</b><br>
       Categoria: ${produto.categoria}<br>
-      ${produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>
+      ${produto.preco.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      })}<br>
       <button> Comprar </button>
   `;
 
   grid.append(criaEl);
 
-  let btn = criaEl.querySelector('button');
-  btn.addEventListener('click', () => {
+  let btn = criaEl.querySelector("button");
+  btn.addEventListener("click", () => {
     carrinho.push({
-          id: produto.id,
-          nome: produto.nome,
-          sku: produto.sku,
-          preco: produto.preco,
-          categoria: produto.categoria,
-          imagem: produto.imagem,
-          quantidade: produto.quantidade || 1,
-      });
+      id: produto.id,
+      nome: produto.nome,
+      sku: produto.sku,
+      preco: produto.preco,
+      categoria: produto.categoria,
+      imagem: produto.imagem,
+      quantidade: produto.quantidade || 1,
+    });
 
-      atualizaCarrinho();
+    atualizaCarrinho();
+
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Produto adicionado com sucesso",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   });
 }
 
 function atualizaCarrinho() {
-    let carrinhoArmazenado = JSON.stringify(carrinho);
-    sessionStorage.setItem('carrinho', carrinhoArmazenado);
+  let carrinhoArmazenado = JSON.stringify(carrinho);
+  sessionStorage.setItem("carrinho", carrinhoArmazenado);
 }
 
-for (let i of produtos) {
-    renderizaProduto(i);
-}
+/* Lais pelo o que eu vi esse trecho de código estava carregando
+os produtos 2x */
 
+// for (let i of produtos) {
+//     renderizaProduto(i);
+// }
 
 // função o filtro
 
 function filtraPorCategoria(categoriaSelecionada) {
-    
-  grid.innerHTML = '';
+  grid.innerHTML = "";
 
   if (categoriaSelecionada) {
-        const produtosFiltrados = produtos.filter(function(produto) {
-            return produto.categoria === categoriaSelecionada;
-        });
+    const produtosFiltrados = produtos.filter(function (produto) {
+      return produto.categoria === categoriaSelecionada;
+    });
 
-        produtosFiltrados.forEach(function(produto) {
-            renderizaProduto(produto);
-        });
-    } else {
-        produtos.forEach(function(produto) {
-            renderizaProduto(produto);
-        });
-    }
+    produtosFiltrados.forEach(function (produto) {
+      renderizaProduto(produto);
+    });
+  } else {
+    produtos.forEach(function (produto) {
+      renderizaProduto(produto);
+    });
+  }
 }
 
-produtos.forEach(function(produto) {
-    renderizaProduto(produto);
+produtos.forEach(function (produto) {
+  renderizaProduto(produto);
 });
 
-document.getElementById('filtroCategoria').addEventListener('change', function(event) {
+document
+  .getElementById("filtroCategoria")
+  .addEventListener("change", function (event) {
     const categoriaSelecionada = event.target.value;
     filtraPorCategoria(categoriaSelecionada);
-});
-
-  
+  });
