1f65623bfda32d24e7a0d31f24d53e970057706a
lucasvida <lucasvida@gmail.com>
2024-02-01 13:20:30 -0300

    Inclusão do Menu Móbile

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,1 +1,18 @@
 //JS Comum para todo site
+
+let menu = document.querySelector('.btn-menu');
+let menuOffCanvas = document.querySelector('.menu-off-canvas');
+let menuClose = document.querySelector('.close-menu');
+
+function abreMenu(){
+    menuOffCanvas.style.cssText = "margin:0;";
+  }
+  
+
+function fechaMenu(){
+  menuOffCanvas.style.cssText = "margin-left: -300px;";
+}
+
+ 
+menu.addEventListener("click",abreMenu);
+menuClose.addEventListener("click",fechaMenu);
