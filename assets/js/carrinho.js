dcda5c7f1c03c57322a02bb3b24cbdd8ed83e9c2
Lucas Vida <lucasvida@gmail.com>
2024-02-03 11:09:24 -0300

    Ajuste no Carrinho | Soma do valor dos itens

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -8,7 +8,6 @@ const itensCarrinhoQuantidade = document.querySelector(".icone-carrinho");
 let somaPrecos = 0;
 let totalItens = 0;
 
-
 window.addEventListener("load", () => {
   carrinhoGerado != null ? renderizaCarrinho() : alert("vazio");
 });
@@ -18,9 +17,7 @@ function renderizaCarrinho() {
     carrinhoCompras.innerHTML += `
         <td>
             <div class="produto-listado">
-            <img src="${produto.imagem}" alt="${
-      produto.nome
-    } title="${
+            <img src="${produto.imagem}" alt="${produto.nome} title="${
       produto.nome
     }" class="img-tumb-carrinho">
                 <div class="info-produto">
@@ -35,9 +32,17 @@ function renderizaCarrinho() {
               currency: "BRL",
             })}</span>
             <div class="seletor-quantidade">
-          <button type="button" class="btn-quantidade" id="btn-menos-${produto.id}">-</button>
-              <input type="text" value="${produto.quantidade}" class="input-quantidade" id="input-quantidade-${produto.id}" inputmode="numeric">
-          <button type="button" class="btn-quantidade" id="btn-mais-${produto.id}">+</button>
+          <button type="button" class="btn-quantidade" id="btn-menos-${
+            produto.id
+          }" aria-label="Reduz itens do produto ${produto.nome}">-</button>
+              <input type="text" value="${
+                produto.quantidade
+              }" class="input-quantidade" id="input-quantidade-${
+      produto.id
+    }" inputmode="numeric" readonly="true">
+          <button type="button" class="btn-quantidade" id="btn-mais-${
+            produto.id
+          }" aria-label="Adiciona itens do produto ${produto.nome}">+</button>
         </div>
             </div>
                 </div>
@@ -50,46 +55,66 @@ function renderizaCarrinho() {
 
     let btnMenos = document.querySelector(`#btn-menos-${produto.id}`);
     let btnMais = document.querySelector(`#btn-mais-${produto.id}`);
-    let inputQuantidade = document.querySelector(`#input-quantidade-${produto.id}`);
-    
-    btnMenos.addEventListener('click',()=>{
-      if( inputQuantidade.value > 0){
+    let inputQuantidade = document.querySelector(
+      `#input-quantidade-${produto.id}`
+    );
+
+    btnMenos.addEventListener("click", () => {
+      if (inputQuantidade.value > 0) {
         inputQuantidade.value = parseInt(inputQuantidade.value) - 1;
         produto.quantidade = parseInt(inputQuantidade.value);
         totalItensCarrinho();
+        totalItensSoma();
+        if (inputQuantidade.value <= 0) {
+          alert("remover");
+        }
       }
-    })
-    
-    btnMais.addEventListener('click',()=>{
+    });
+
+    btnMais.addEventListener("click", () => {
       inputQuantidade.value = parseInt(inputQuantidade.value) + 1;
       produto.quantidade = parseInt(inputQuantidade.value);
       totalItensCarrinho();
-    })
+      totalItensSoma();
+    });
   }
-
-
 }
+
+//Verifica o total de Itens
+function totalItensCarrinho() {
+  totalItens = 0;
+  for (let i = 0; i < carrinhoGerado.length; i++) {
+    totalItens += carrinhoGerado[i].quantidade;
+  }
+  subtotalItens.innerText = totalItens;
+  itensCarrinhoQuantidade.innerText = totalItens;
+}
+
+//Verifica o total da Soma
+function totalItensSoma() {
+  somaPrecos = 0;
+  for (let i = 0; i < carrinhoGerado.length; i++) {
+    somaPrecos += carrinhoGerado[i].preco * carrinhoGerado[i].quantidade;
+  }
+  subtotal.innerHTML = `<div class="valor-da-compra">${somaPrecos.toLocaleString(
+    "pt-BR",
+    {
+      style: "currency",
+      currency: "BRL",
+    }
+  )}</div>`;
+}
+
+//Fazer um if
+itensCarrinhoQuantidade.innerText = carrinhoGerado.length;
 
 for (let i = 0; i < carrinhoGerado.length; i++) {
   somaPrecos += carrinhoGerado[i].preco * carrinhoGerado[i].quantidade;
 }
 
-//Verifica o total de Itens
-function totalItensCarrinho(){
-  totalItens = 0;
-  for (let i = 0; i < carrinhoGerado.length; i++){
-    totalItens += carrinhoGerado[i].quantidade;
-  }
-  subtotalItens.innerText = totalItens;
-}
-
-//Fazer um if
-itensCarrinhoQuantidade.innerText = totalItens;
-
 //Inicializa o Carrinho com quantidade básica de itens.
 subtotalItens.innerText = carrinhoGerado.length;
 subtotalItens.classList.add("valor-da-compra");
-
 
 subtotal.innerHTML = `<div class="valor-da-compra">${somaPrecos.toLocaleString(
   "pt-BR",
